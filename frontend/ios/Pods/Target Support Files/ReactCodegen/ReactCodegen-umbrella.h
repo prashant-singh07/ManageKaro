b2fca40bc5706866261edcacc0461f73eab5380e
prashant <prashant.singh@paralleldots.com>
2025-04-15 00:44:17 +0530

    Add frontend directory with initial setup

---
+++ frontend/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -0,0 +1,60 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "RCTModulesConformingToProtocolsProvider.h"
+#import "RCTThirdPartyComponentsProvider.h"
+#import "react/renderer/components/RNCSlider/ComponentDescriptors.h"
+#import "react/renderer/components/RNCSlider/EventEmitters.h"
+#import "react/renderer/components/RNCSlider/Props.h"
+#import "react/renderer/components/RNCSlider/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNCSlider/ShadowNodes.h"
+#import "react/renderer/components/RNCSlider/States.h"
+#import "react/renderer/components/RNDatePickerSpecs/ComponentDescriptors.h"
+#import "react/renderer/components/RNDatePickerSpecs/EventEmitters.h"
+#import "react/renderer/components/RNDatePickerSpecs/Props.h"
+#import "react/renderer/components/RNDatePickerSpecs/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNDatePickerSpecs/ShadowNodes.h"
+#import "react/renderer/components/RNDatePickerSpecs/States.h"
+#import "react/renderer/components/RNDateTimePickerCGen/ComponentDescriptors.h"
+#import "react/renderer/components/RNDateTimePickerCGen/EventEmitters.h"
+#import "react/renderer/components/RNDateTimePickerCGen/Props.h"
+#import "react/renderer/components/RNDateTimePickerCGen/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNDateTimePickerCGen/ShadowNodes.h"
+#import "react/renderer/components/RNDateTimePickerCGen/States.h"
+#import "react/renderer/components/rnscreens/ComponentDescriptors.h"
+#import "react/renderer/components/rnscreens/EventEmitters.h"
+#import "react/renderer/components/rnscreens/Props.h"
+#import "react/renderer/components/rnscreens/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rnscreens/ShadowNodes.h"
+#import "react/renderer/components/rnscreens/States.h"
+#import "react/renderer/components/safeareacontext/ComponentDescriptors.h"
+#import "react/renderer/components/safeareacontext/EventEmitters.h"
+#import "react/renderer/components/safeareacontext/Props.h"
+#import "react/renderer/components/safeareacontext/RCTComponentViewHelpers.h"
+#import "react/renderer/components/safeareacontext/ShadowNodes.h"
+#import "react/renderer/components/safeareacontext/States.h"
+#import "RNBootSplashSpec/RNBootSplashSpec.h"
+#import "RNBootSplashSpecJSI.h"
+#import "RNDatePickerSpecs/RNDatePickerSpecs.h"
+#import "RNDatePickerSpecsJSI.h"
+#import "RNDateTimePickerCGen/RNDateTimePickerCGen.h"
+#import "RNDateTimePickerCGenJSI.h"
+#import "RNImagePickerSpec/RNImagePickerSpec.h"
+#import "RNImagePickerSpecJSI.h"
+#import "rnscreens/rnscreens.h"
+#import "rnscreensJSI.h"
+#import "safeareacontext/safeareacontext.h"
+#import "safeareacontextJSI.h"
+
+FOUNDATION_EXPORT double ReactCodegenVersionNumber;
+FOUNDATION_EXPORT const unsigned char ReactCodegenVersionString[];
+
